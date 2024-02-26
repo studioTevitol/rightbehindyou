ce814ecd5b408ab5bb220179285076bbd8f3fe0b
N0D3V <56217749+N0-D3V@users.noreply.github.com>
2024-02-26 09:41:43 +0300

    Fixed spawning issues with key gameobject.

--- Right Behind You/Assets/Scripts/SpawnObject.cs
+++ Right Behind You/Assets/Scripts/SpawnObject.cs
@@ -7,29 +7,35 @@ public class SpawnObject : MonoBehaviour
     public GameObject Keyprefab;
     public Vector3 center;
     public Vector3 size;
+    Mesh mesh;
+    public Transform mazecorner1;
+    public Transform mazecorner2;
     // Start is called before the first frame update
     void Start()
     {
-        SpawnKey();
-        SpawnKey();
         SpawnKey();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Collider[] walls = Physics.OverlapSphere(transform.position, size.x);
+        if (walls.Length > 0)
+        {
+            for (int i = 0; i < walls.Length; i++)
+            {
+                if (walls[i].gameObject.tag=="Wall")
+                {
+                    transform.position = new Vector3(transform.position.x + 0.25f, transform.position.y, transform.position.z);
+                }
+            }
+        }
     }
 
     public void SpawnKey()
     {
-        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2 , size.x /2),Random.Range(-size.y / 2 , size.y /2),Random.Range(-size.z / 2 , size.z /2));
+        Vector3 pos = center + new Vector3(Random.Range(mazecorner1.position.x , mazecorner2.position.x),Random.Range(mazecorner1.position.y, mazecorner2.position.y),Random.Range(mazecorner1.position.z, mazecorner2.position.z));
+        Keyprefab.transform.localScale.Scale(size);
         Instantiate(Keyprefab, pos, Quaternion.identity);
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.color = new Color(1,0,0,0.5f);
-        Gizmos.DrawCube(center, size);
     }
 }
